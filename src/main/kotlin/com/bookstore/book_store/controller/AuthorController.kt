7cd6a10858696aabb25e74422e85c31877b934a6
Abid Hasan <developer.abid@gmail.com>
2019-10-03 09:55:27 +0600

    separated service and implementation
    - updated author controller with service
    - added version to api url

--- src/main/kotlin/com/bookstore/book_store/controller/AuthorController.kt
+++ src/main/kotlin/com/bookstore/book_store/controller/AuthorController.kt
@@ -4,26 +4,16 @@ import com.bookstore.book_store.model.Author
 import com.bookstore.book_store.repository.AuthorRepository
 import com.bookstore.book_store.service.AuthorService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Sort
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.awt.print.Pageable
 import java.util.*
 
 
 @RestController
-@RequestMapping("/authors")
-class AuthorController(@Autowired var authorRepo: AuthorRepository,
-                       @Autowired var authorService: AuthorService) {
-
-
+@RequestMapping("/v1/authors")
+class AuthorController(@Autowired var authorRepo: AuthorRepository, @Autowired var authorService: AuthorService) {
     @GetMapping
-    fun getAllAuthor(size: Optional<Int>,
-                     sortBy: Optional<String>,
-                     name: Optional<String>
-    ): ResponseEntity<List<Author>> {
+    fun getAllAuthor(size: Optional<Int>, sortBy: Optional<String>, name: Optional<String>): ResponseEntity<List<Author>> {
         return authorService.getAllAuthors(name = name.orElse("_"), sortBy = sortBy.orElse("id"),
                 limit = size.orElse(Int.MAX_VALUE))
     }
@@ -35,14 +25,12 @@ class AuthorController(@Autowired var authorRepo: AuthorRepository,
 
     @PostMapping
     fun addAuthor(@RequestBody author: Author): ResponseEntity<Author> {
-        authorRepo.save(author)
-        return ResponseEntity(authorRepo.getOne(author.id), HttpStatus.CREATED) // status code 201
+        return authorService.addAuthor(author)
     }
 
     @PutMapping("/{id}")
     fun updateAuthor(@PathVariable id: Long, @RequestBody author: Author): ResponseEntity<Author> {
-        return authorService.updateAuthor(id,author)
-        // partial update
+        return authorService.updateAuthor(id, author)
     }
 
     @DeleteMapping("/{id}")
--- src/main/kotlin/com/bookstore/book_store/service/AuthorService.kt
+++ src/main/kotlin/com/bookstore/book_store/service/AuthorService.kt
@@ -1,32 +1,13 @@
 package com.bookstore.book_store.service
 
 import com.bookstore.book_store.model.Author
-import com.bookstore.book_store.repository.AuthorRepository
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Sort
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Service
 
-@Service
-class AuthorService(@Autowired var authorRepo: AuthorRepository) {
-    fun getAllAuthors(name: String, sortBy: String, limit: Int): ResponseEntity<List<Author>> {
-        val pageable = PageRequest.of(0, limit, Sort.by(sortBy).ascending());
-        return ResponseEntity(authorRepo.findAuthors(name, pageable), HttpStatus.OK)
-    }
 
-    fun getAuthor(id: Long): ResponseEntity<Author> {
-        return ResponseEntity(authorRepo.getOne(id), HttpStatus.OK)
-    }
-
-    fun updateAuthor(id: Long, author: Author): ResponseEntity<Author> {
-        if (authorRepo.getOne(id) != null) {
-            authorRepo.save(author)
-            return ResponseEntity(authorRepo.getOne(id), HttpStatus.OK) // status code 201
-        } else {
-            return ResponseEntity(HttpStatus.BAD_REQUEST) // status code 201
-        }
-    }
-
+interface AuthorService {
+    fun getAllAuthors(name: String, sortBy: String, limit: Int): ResponseEntity<List<Author>>
+    fun getAuthor(id: Long): ResponseEntity<Author>
+    fun updateAuthor(id: Long, author: Author): ResponseEntity<Author>
+    fun addAuthor(author: Author): ResponseEntity<Author>
+    fun deleteAuthor(id: Long): ResponseEntity<String>
 }
